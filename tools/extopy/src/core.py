0fd35dcce320ce1dbd0b7b84feb70869dba3c5bd
ykyki <yukimath2@gmail.com>
2021-10-09 18:39:01 +0900

    feat(extopy): install mypy

--- tools/extopy/src/core.py
+++ tools/extopy/src/core.py
@@ -4,5 +4,5 @@ import click
 @click.command()
 @click.argument('target')
 @click.option('--layout', '-l', default='develop', help='レイアウト指定')
-def build_by_path(target, layout):
+def build_by_path(target: str, layout: str) -> None:
     print(f'{target=}, {layout=}')
