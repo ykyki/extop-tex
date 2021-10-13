b971c817a1cad47ed1be78cd41efaaa1bdd9b77c
ykyki <yukimath2@gmail.com>
2021-10-13 22:16:22 +0900

    refactor(extopy): fix is_in_git_repository

--- tools/extopy/src/git.py
+++ tools/extopy/src/git.py
@@ -3,14 +3,16 @@ import typing
 
 
 def is_in_git_repository() -> typing.Tuple[bool, str]:
-    try:
-        # noinspection SpellCheckingInspection
-        output = subprocess.run(
-            ['git', 'rev-parse', '--show-toplevel'],
-            encoding='utf-8',
-            capture_output=True,
-            check=True
-        )
-        return True, output.stdout.strip()
-    except subprocess.CalledProcessError as ex:
-        return False, ex.stderr.strip()
+    # try:
+    #     # noinspection SpellCheckingInspection
+    #     output = subprocess.run(
+    #         ['git', 'rev-parse', '--show-toplevel'],
+    #         encoding='utf-8',
+    #         capture_output=True,
+    #         check=True
+    #     )
+    #     return True, output.stdout.strip()
+    # except subprocess.CalledProcessError as ex:
+    #     return False, ex.stderr.strip()
+    exitcode, output = subprocess.getstatusoutput('git rev-parse --show-toplevel')
+    return exitcode == 0, output
