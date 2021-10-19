ffb251d6f4e0773153d2e9d306fe886ff66c9c67
ykyki <yukimath2@gmail.com>
2021-10-20 00:27:52 +0900

    create initial core.build()

--- tools/extopy/src/core.py
+++ tools/extopy/src/core.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 
 from src import git, lib
@@ -27,4 +29,48 @@ def test():
 @click.argument('target')
 @click.option('--layout', '-l', default='develop', help='レイアウト指定')
 def build(target: str, layout: str) -> None:
+    import subprocess
+
     click.echo(f'{target=}, {layout=}')
+
+    repository_path = git.get_current_repository_root_path()
+
+    target_path = Path(target).resolve()
+    target_path_rel = target_path.relative_to(repository_path)
+    target_stem = target_path.stem
+
+    latexmkrc_path_rel = Path('documents/catalog/.latexmkrc')
+
+    output_dir_rel = Path('temp/')
+    output_dir = repository_path / output_dir_rel
+
+    if not (target_path.exists()):
+        _echo_error(f'File not found: {target_path_rel}')
+        return
+    if not ((repository_path / latexmkrc_path_rel).exists()):
+        _echo_error(f'File not found: {latexmkrc_path_rel}')
+        return
+    if not (output_dir.exists()):
+        _echo_error(f'File not found: {output_dir}')
+        return
+
+    @lib.run_in_tempdir
+    def build_runner():
+        tempdir_path = lib.get_cwd_path()
+
+        git.clone(repository_path, tempdir_path)
+        subprocess.check_output(
+            ['latexmk', '-cd', '-norc', '-r', str(latexmkrc_path_rel), str(target_path_rel)]
+        )
+
+        import shutil
+        shutil.move(
+            target_path_rel.with_suffix('.pdf'),
+            output_dir / Path(f'{target_stem}-from-temp').with_suffix('.pdf')
+        )
+
+    build_runner()
+
+
+def _echo_error(message: str) -> None:
+    click.echo(click.style(message, fg='red'))
