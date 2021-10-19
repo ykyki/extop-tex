26574755e0aa8da279017c0558236fce21dac756
ykyki <yukimath2@gmail.com>
2021-10-20 00:27:26 +0900

    add src.git.clone() method

--- tools/extopy/src/git.py
+++ tools/extopy/src/git.py
@@ -4,4 +4,13 @@ from pathlib import Path
 
 def get_current_repository_root_path() -> Path:
     output = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], encoding='utf-8')
-    return Path(output.strip()).absolute()
+    return Path(output.strip()).resolve()
+
+
+def clone(from_: Path, to_: Path) -> None:
+    # TODO: fix
+    assert from_.is_absolute()
+    assert to_.is_absolute()
+
+    subprocess.check_output(['git', 'clone', str(from_), str(to_)], encoding='utf-8')
+    return
