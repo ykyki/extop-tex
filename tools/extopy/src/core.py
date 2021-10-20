fe7ad6c96cae3d621bd9bf232f90034d8b0d103d
ykyki <yukimath2@gmail.com>
2021-10-20 23:35:10 +0900

    implement --layout option of extopy build

--- tools/extopy/src/core.py
+++ tools/extopy/src/core.py
@@ -27,7 +27,10 @@ def test():
 
 @cli.command(help='Build PDF file')
 @click.argument('target')
-@click.option('--layout', '-l', default='develop', help='レイアウト指定')
+@click.option('--layout', '-l',
+              type=click.Choice(['product', 'develop', 'review']),
+              default='product',
+              help='Layout setting (default = product)')
 def build(target: str, layout: str) -> None:
     import subprocess
 
@@ -45,6 +48,8 @@ def build(target: str, layout: str) -> None:
 
     if not (target_path.exists()):
         raise FileNotFoundError(f'{target_path_rel}')
+    if not (target_path.suffix == '.tex'):
+        raise ValueError(f'Illegal suffix: {target_path_rel.name}')
     if not ((repository_path / latexmkrc_path_rel).exists()):
         raise FileNotFoundError(f'{latexmkrc_path_rel}')
     if not (output_dir.exists()):
@@ -56,15 +61,24 @@ def build(target: str, layout: str) -> None:
 
         git.clone(repository_path, tempdir_path)
 
+        with open(tempdir_path / target_path_rel, mode='r', encoding='utf-8') as file:
+            file_content = file.read()
+        with open(tempdir_path / target_path_rel, mode='w', encoding='utf-8') as file:
+            # noinspection SpellCheckingInspection
+            replaced = file_content.replace(r'\begin{document}', rf'\setlayout{{{layout}}}\begin{{document}}', 1)
+            file.write(replaced)
+        del file_content
+        del replaced
+
         # noinspection SpellCheckingInspection
         subprocess.check_output(
-            ['latexmk', '-cd', '-norc', '-r', str(latexmkrc_path_rel), str(target_path_rel)]
+            ['latexmk', '-cd', '-norc', '-r', str(latexmkrc_path_rel), str(target_path_rel)],
         )
 
         import shutil
         shutil.move(
             target_path_rel.with_suffix('.pdf'),
-            output_dir / Path(f'{target_path.stem}-from-temp').with_suffix('.pdf')
+            output_dir / Path(f'{target_path.stem}-{layout}').with_suffix('.pdf')
         )
 
     build_runner()
