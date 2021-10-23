12338ef784f16693d244d28b9df0cbf3ce96ab6c
ykyki <yukimath2@gmail.com>
2021-10-23 14:08:00 +0900

    create git.revision()

--- tools/extopy/src/git.py
+++ tools/extopy/src/git.py
@@ -8,10 +8,19 @@ def get_current_repository_path() -> Path:
 
 
 def clone(from_: Path, to_: Path) -> None:
-    if not(from_.is_absolute()):
+    if not (from_.is_absolute()):
         raise ValueError(f'{from_=} should be an absolute path.')
-    if not(to_.is_absolute()):
+    if not (to_.is_absolute()):
         raise ValueError(f'{to_=} should be an absolute path.')
 
     subprocess.check_output(['git', 'clone', str(from_), str(to_)], encoding='utf-8')
     return
+
+
+def revision(expr: str, is_short: bool = False) -> str:
+    if is_short:
+        output = subprocess.check_output(['git', 'rev-parse', '--verify', '--short', expr], encoding='utf-8')
+    else:
+        output = subprocess.check_output(['git', 'rev-parse', '--verify', expr], encoding='utf-8')
+
+    return output.strip()
--- tools/extopy/test/test_git.py
+++ tools/extopy/test/test_git.py
@@ -16,3 +16,14 @@ def test_get_current_repository_path_in_tempdir():
         assert False
     except subprocess.CalledProcessError:
         assert True
+
+
+# noinspection PyPep8Naming
+def test_revision():
+    SHA1_LENGTH = 40
+    SHA1_SHORT_LENGTH = 7
+
+    rev = revision('HEAD')
+    assert len(rev) == SHA1_LENGTH
+    rev = revision('HEAD', is_short=True)
+    assert len(rev) == SHA1_SHORT_LENGTH
