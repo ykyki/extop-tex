161d6dda6556489b8cf1c05881dfda61903b87c6
ykyki <yukimath2@gmail.com>
2022-03-06 22:00:37 +0900

    update build-by-path.sh

--- tools/build-by-path.sh
+++ tools/build-by-path.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 # set -x
-set -e
+set -eu
 
 REPO_DIR=$(cd "$(git rev-parse --show-toplevel)" && pwd -P)
 OUTPUT_DIR=$REPO_DIR/temp
-
-LATEXMKRC_FILE='documents/catalog/.latexmkrc'
 
 LINE_SEPARATOR='----- ----- ----- ----- ----- ----- ----- ----- '
 
 DOC_LAYOUT='product'
 REPO_VER='HEAD'
 
-if [ $# = 1 ]; then
+if [ $# -eq 1 ]; then
   TARGET_PATH=$1
-elif [ $# = 2 ]; then
+elif [ $# -eq 2 ]; then
   TARGET_PATH=$1
   DOC_LAYOUT=$2
-elif [ $# = 3 ]; then
+elif [ $# -eq 3 ]; then
   TARGET_PATH=$1
   DOC_LAYOUT=$2
   REPO_VER=$3
 else
-  echo 'usage: build-by-path.sh [path] [layout (default = product)] [version (default = HEAD)]'
+  echo 'usage: build-by-path.sh [path] [layout (default = product)] [version (default = HEAD)]' >&2
   exit 1
 fi
 
@@ -32,16 +30,24 @@ TARGET_NAME=$(basename "$TARGET_PATH" ".tex")
 if [ -f "$REPO_DIR/$TARGET_DIR/$TARGET_NAME.tex" ]; then
   echo "Target path: [repository root]/$TARGET_PATH"
 else
-  echo "Invalid path: [repository root]/$TARGET_PATH"
+  echo "Invalid path: [repository root]/$TARGET_PATH" >&2
   exit 1
 fi
 
-case $DOC_LAYOUT in
+LATEXMKRC_FILE='documents/catalog/.latexmkrc'
+if [ -f "$LATEXMKRC_FILE" ]; then
+  echo "Config file: [repository root]/$LATEXMKRC_FILE"
+else
+  echo "Config file not found: [repository root]/$LATEXMKRC_FILE" >&2
+  exit 1
+fi
+
+case "$DOC_LAYOUT" in
 'product' | 'develop' | 'review')
   echo "Layout: $DOC_LAYOUT"
   ;;
 *)
-  echo "Invalid layout: $DOC_LAYOUT"
+  echo "Invalid layout: $DOC_LAYOUT" >&2
   exit 1
   ;;
 esac
@@ -51,10 +57,7 @@ if git rev-parse --verify "$REPO_VER" >/dev/null 2>&1; then
   GIT_HASH_SHORT=$(git rev-parse --short "$REPO_VER")
   echo "Version: $REPO_VER ($GIT_HASH_SHORT)"
 else
-  echo "Invalid version: $REPO_VER"
-  echo
-  echo "Example: HEAD, {hash}, {tag}"
-  echo "To see tags: git tag -l"
+  echo "Invalid version: $REPO_VER" >&2
   exit 1
 fi
 
@@ -80,3 +83,4 @@ rm -rf "$TMP_DIR"
 cd - || exit
 
 exit 0
+
